60df454ae57c437f38e6cc35754c147eba348bb8
deanspecht <deansp@web.de>
2024-03-07 16:57:00 +0100

    add days

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,17 @@ public class Main {
     public static void main(String[] args) {
 
         String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+
         for (int i = 0; i < (months.length); i++) {
             printCalendarForMonth(months[i]);
+
         }
     }
     public static void printCalendarForMonth(String monthName) {
         System.out.println(monthName);
+        System.out.println("Mo Tu We Th Fr Sa Su");
     }
+
+
 }
