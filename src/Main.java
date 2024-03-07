df712d6f2ea6b55970421533ad2f99749e8ae5ce
devchristiansimon <inonlinoid@gmail.com>
2024-03-07 16:03:48 +0100

    warnung vor bitcoin

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Heyho");
+        System.out.println("Kauft kein Bitcoin!!!!111elf");
         System.out.println("Version 2");
         System.out.println("Comment from Dominik");
         System.out.println("Buy Bitcoin - not financial advice");
